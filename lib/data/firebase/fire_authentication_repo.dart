e3e0d6bfa4369033c872c3f04a6544d398128fdf
noobmaster69 <139727372+pichnarin@users.noreply.github.com>
2025-04-17 18:26:51 +0700

    task: firebase data structure and authentication

---
+++ lib/data/firebase/fire_authentication_repo.dart
@@ -0,0 +1,22 @@
+import 'package:race_tracking_app_v1/data/repo/authentication_repo.dart';
+
+class FireAuthenticationRepo extends AuthenticationRepository{
+  @override
+  Future<void> signIn(String email, String password) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp(String email, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+  
+}
---
+++ lib/data/repo/authentication_repo.dart
@@ -0,0 +1,5 @@
+abstract class AuthenticationRepository{
+  Future<void> signIn(String email, String password);
+  Future<void> signUp(String email, String password);
+  Future<void> signOut();
+}
